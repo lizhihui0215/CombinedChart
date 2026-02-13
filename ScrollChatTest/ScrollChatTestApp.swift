99d4111c403224c98ef8b560c562c60ced45f9e0
lizhihui0215 <lizhihui0215@gmail.com>
2026-02-13 15:31:23 +0800

    Initial Commit

---
+++ ScrollChatTest/ScrollChatTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollChatTestApp.swift
+//  ScrollChatTest
+//
+//  Created by Bernard on 2026/2/13.
+//
+
+import SwiftUI
+
+@main
+struct ScrollChatTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScrollChatTestTests/ScrollChatTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollChatTestTests.swift
+//  ScrollChatTestTests
+//
+//  Created by Bernard on 2026/2/13.
+//
+
+import Testing
+@testable import ScrollChatTest
+
+struct ScrollChatTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
