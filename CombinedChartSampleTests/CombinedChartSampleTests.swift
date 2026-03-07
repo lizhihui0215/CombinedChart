7c37fa39be0303beb621343ff76cdcea01540d6b
lizhihui0215 <lizhihui0215@gmail.com>
2026-03-08 07:34:07 +0800

    Apply refactor from SwiftUI pager branch

---
+++ CombinedChartSampleTests/CombinedChartSampleTests.swift
@@ -0,0 +1,15 @@
+//
+//  CombinedChartSampleTests.swift
+//  ScrollChatTestTests
+//
+//  Created by Bernard on 2026/2/13.
+//
+
+@testable import CombinedChartSample
+import Testing
+
+struct CombinedChartSampleTests {
+    @Test func example() {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
